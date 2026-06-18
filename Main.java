fae5f4904d4f7d08ed9ffb78185aaa28259a9b4f
LAB4804WD10 <iikatie103@gmail.com>
2026-06-18 09:08:05 -0700

    Updated code

--- Main.java
+++ Main.java
@@ -1,12 +1,18 @@
-// YOUR NAME: 
+// YOUR NAME: Katelyn Ho
 // COLLABORATORS:  
-// DATE: 
+// DATE: 6/18/2026
 
 public class Main
 {
 	public static void main(String[] args)
 	{
 		// DECLARATION SECTION
+		char state1 = 'c' - 32;
+		char state2 = 'a' - 32;
+		int numberOfDacaRecipients = 700000;
+		long amountInTaxes = 5700000000L;
+		double averageContribution = amountInTaxes / numberOfDacaRecipients;
+		int averageContributionRounded = (int) averageContribution;
 
 		// INITIALIZATION SECTION
 		
@@ -17,6 +23,9 @@ public class Main
 
 		
 		// OUTPUT SECTION
-		
+		System.out.println("Average DACA-recipient Tax Contribution:" + " " + "$" + averageContribution);
+		System.out.println("Rounded down to the nearest whole dollar:" + " " + "$" + averageContributionRounded);
+		System.out.println("State with most DACA recipients:" + " " + state1 + state2);
+
 	}
 }
