7ded83b2307c252f18ef3e8e5b480bb91c8dfea1
LAB4804WD10 <iikatie103@gmail.com>
2026-06-18 09:32:06 -0700

    Updated code again

--- Main.java
+++ Main.java
@@ -7,25 +7,28 @@ public class Main
 	public static void main(String[] args)
 	{
 		// DECLARATION SECTION
-		char state1 = 'c' - 32;
-		char state2 = 'a' - 32;
+		char state1 = 'c';
+		char state2 = 'a';
+		String stateMostDaca;
 		int numberOfDacaRecipients = 700000;
-		long amountInTaxes = 5700000000L;
-		double averageContribution = amountInTaxes / numberOfDacaRecipients;
+		long amountInTaxes = 60000000000L;
+		double averageContribution = (double) amountInTaxes / numberOfDacaRecipients;
 		int averageContributionRounded = (int) averageContribution;
 
 		// INITIALIZATION SECTION
-		
+
 		// INPUT SECTION
 		// N/A (no input for this lab)
 		
 		// CALCULATION & PROCESSING SECTION
-
+		state1 = (char) (state1 - 32);
+		state2 = (char) (state2 - 32);
+		stateMostDaca = state1 + "" + state2;
 		
 		// OUTPUT SECTION
 		System.out.println("Average DACA-recipient Tax Contribution:" + " " + "$" + averageContribution);
 		System.out.println("Rounded down to the nearest whole dollar:" + " " + "$" + averageContributionRounded);
-		System.out.println("State with most DACA recipients:" + " " + state1 + state2);
+		System.out.println("State with most DACA recipients:" + " " + stateMostDaca);
 
 	}
 }
